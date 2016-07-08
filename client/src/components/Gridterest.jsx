a3bda0b0efeafc9666109ff1f47a2468a883892c
Donald Lessells <donald.lessells@gmail.com>
2016-07-08 18:55:30 +0100

    added methods for checking validity of tile group selections

--- client/src/components/Gridterest.jsx
+++ client/src/components/Gridterest.jsx
@@ -62,6 +62,79 @@ const Gridterest = React.createClass({
     } else if (position % 5 === 0) {
       return false;
     } else return position + 1;
+  },
+
+  adjacent: function (position1, position2) {
+    if (
+      (adjacentTop(position1) === position2) ||
+      (adjacentBottom(position1) === position2) ||
+      (adjacentLeft(position1) === position2) ||
+      (adjacentRight(position1) === position2)
+    ) {
+      return true;
+    }
+  },
+
+  singleTile: function (array) {
+    if (array.length === 1) {
+      return true;
+    }
+  },
+
+  twoAdjacentTiles: function (array) {
+    if (array.length != 2) {
+      return false;
+    } else if (adjacent(array[0], array[1])) {return true;}
+  },
+
+  threeAdjacentTiles: function (array) {
+    if (array.length != 3) {
+      return false;
+    } else if (!(twoAdjacentTiles(array[0], array[1]))) {
+      return false;
+    } else if (
+      (array[1] === adjacentBottom(array[0])) && (array[2] === adjacentBottom(array[1])) ||
+      (array[1] === adjacentRight(array[0])) && (array[2] === adjacentRight(array[1]))
+    ) {
+      return true;
+    } else return false;
+  },
+
+  fourAdjacentTiles: function (array) {
+    if (array.length != 4) {
+      return false;
+    } else if (!(threeAdjacentTiles([array[0], array[1], array[2]]))) {
+      return false;
+    } else if (
+      (array[2] === adjacentBottom(array[1])) && (array[3] === adjacentBottom(array[2])) ||
+      (array[2] === adjacentRight(array[1])) && (array[3] === adjacentRight(array[2]))
+    ) {
+      return true;
+    } else return false;
+  },
+
+  twoByTwoSquare: function (array) {
+    if (array.length != 4) {
+      return false;
+    } else if (
+      (adjacent(array[0], array[1])) &&
+      (adjacent(array[2], array[3])) &&
+      (adjacentBottom(array[0]) === array[2])
+    ) {
+      return true;
+    } else return false;
+  },
+
+  validSelectionGroup: function (array) {
+    if (
+      singleTile(array) ||
+      twoAdjacentTiles(array) ||
+      threeAdjacentTiles(array) ||
+      fourAdjacentTiles(array) ||
+      twoByTwoSquare(array)
+    ) {
+      return true;
+    } else return false;
   }
 
 });
